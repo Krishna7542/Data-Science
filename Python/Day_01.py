943bef7a55bec02b80a144ed977df3c05557a0a1
kaypii75 <krishnapandey7542@gmail.com>
2026-06-16 19:58:27 +0530

    Day 1

---
+++ Python/Day_01.py
@@ -0,0 +1,45 @@
+#Find the largest number in the list without using max()
+# l = [10, 25, 5, 40, 15]
+# largest = 0
+
+# for i in l:
+#     if i > largest:
+#         largest = i
+# print(largest)
+
+
+# Count the total vowels in the following string
+# s = "krishnapandey"
+# vowel = "aeiou"
+# count = 0
+
+# for el in s:
+#     if el in vowel:
+#         count += 1
+# print(count)
+
+
+# Reverse a string without using slicing ([::-1])
+# s = "Python"
+# r = ""
+
+# for el in s:
+#     r = el + r
+
+# print(r)
+
+
+#Find duplicate elements in the list without using set() and collection.counter.
+# l = [1, 2, 3, 2, 4, 5, 1]
+
+# unique = []
+# duplicate = []
+
+# for i in l:
+#     if i in unique:
+#         if i not in duplicate:
+#             duplicate.append(i)
+#     else:
+#         unique.append(i)
+
+# print(duplicate)

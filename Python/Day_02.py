a15e2ccacb945e447be4d0abec61638463f86924
kaypii75 <krishnapandey7542@gmail.com>
2026-06-17 16:39:58 +0530

    Day_02

--- Python/Day_02.py
+++ Python/Day_02.py
@@ -1,1 +1,41 @@
-print("hello")
+# Count odd numbers in a list
+# l = [10, 25, 5, 40, 15, 8, 12]
+# count = 0
+# for i in l:
+#     if i %2 != 0:
+#         count += 1
+# print(count)
+
+
+# Print all numbers greater than 20
+# l = [10, 25, 5, 40, 15, 8, 12]
+# for i in l:
+#     if i > 20:
+#         print("numbers greater than 20 = ",i)
+
+
+# # Find the smallest number without using min()
+# l = [10, 25, 5, 40, 15]
+# smallest = l[0]
+# for i in l:
+#     if i < smallest:
+#         smallest = i
+# print(smallest)
+
+# Count Vowels in a String
+# s = "programming"
+# vowels = "aeiou"
+# count = 0
+# for el in s:
+#     if el in vowels:
+#         count += 1
+# print("Total Vowels in a String =",count)
+
+
+# Sum of All Elements in a List
+# l = [10, 20, 30, 40]
+# sum = 0
+# for i in l:
+#     sum += i
+# print("Sum of List l =",sum)
+
